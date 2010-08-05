fdbe9a20630c469c7c804b2e1c189209f389f488
par117 <par117@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2010-08-05 16:22:35 +0000

    adding the content types... lots of untested code

--- huckInstitutes/Huck/HuckEntities/trunk/Products/HuckEntities/__init__.py
+++ huckInstitutes/Huck/HuckEntities/trunk/Products/HuckEntities/__init__.py
@@ -1,5 +1,33 @@
 from zope.i18nmessageid import MessageFactory
+from Products.Archetypes import atapi
+from Products.CMFCore import utils
+from Products.CMFCore.permissions import setDefaultRoles
+
 HuckEntitiesMessageFactory = MessageFactory('Products.HuckEntities')
 
+import institute
+import center
+import graduate_program
+import facility
+import equipment
+
+# TODO: do I need this?
+ADD_PERMISSIONS = {
+    'SimpleNewsItem': 'SimpleNewsItem: Add SimpleNewsItem',
+    'CaseStudy': 'CaseStudy: Add CaseStudy'
+}
+
+PRODUCT_NAME = 'HuckEntities'
+
 def initialize(context):
-    pass
+    content_types, constructors, ftis = atapi.process_types(
+        atapi.listTypes(PRODUCT_NAME),
+        PRODUCT_NAME)
+
+    for atype, constructor in zip(content_types, constructors):
+        utils.ContentInit('%s: %s' % (PRODUCT_NAME, atype.portal_type),
+            content_types      = (atype,),
+            # TODO: do I need this next line?
+            permission         = ADD_PERMISSIONS[atype.portal_type],
+            extra_constructors = (constructor,),
+            ).initialize(context)
