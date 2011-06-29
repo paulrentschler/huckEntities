e6ad6c4f63330991701c488cd1a6fa64b37acca8
par117 <par117@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2011-06-29 15:55:05 +0000

    tagging version 1.2

---
+++ huckInstitutes/Huck/HuckEntities/tags/1.2/Products/HuckEntities/center.py
@@ -0,0 +1,27 @@
+"""Definition of the Center content type
+"""
+
+from zope.interface import implements
+from Products.Archetypes import atapi
+
+from Products.HuckEntities import HuckEntitiesMessageFactory as _
+from Products.HuckEntities.interfaces import ICenter
+from Products.HuckEntities.institute import InstituteSchema, Institute
+
+
+CenterSchema = InstituteSchema.copy() + atapi.Schema((
+
+))
+
+schemata.finalizeATCTSchema(CenterSchema, moveDiscussion = False)
+
+class Center(Institute):
+    """A Center"""
+    implements(ICenter)
+    
+    meta_type = "Center"
+    schema = CenterSchema
+
+
+
+atapi.registerType(Center, 'HuckEntities')
